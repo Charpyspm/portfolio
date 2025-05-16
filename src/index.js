e6e639e5ae3d14c848e5ad03eeddb8736eb27e0f
Charpyspm <mcharpentier83@icloud.com>
2025-05-16 23:03:51 +0200

    créaton du footer, mise en place des liens, et du style

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

9b5a0095642027ece0c6e69b2b702d300cfca279
Charpyspm <mcharpentier83@icloud.com>
2025-05-16 21:26:48 +0200

    remise a niveau

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 import HOME from './pages/home.jsx';
 import SERVICES from './pages/services.jsx';
@@ -10,24 +10,57 @@ import MENTIONS_LEGALES from './pages/mentions.jsx';
 
 export default function App() {
   return (
-    <div className="Portfolio">
-      <nav>
-        <p>JOHN DOE</p>
-        <Link to='/'>HOME</Link>
-        <Link to='/services'>SERVICES</Link>
-        <Link to='/portfolio'>PORTFOLIO</Link>
-        <Link to='/contact'>CONTACT</Link>
-        <Link to='/mentions'>MENTIONS LEGALES</Link>
+    <div className="App">
+      <div className="header text-left ">
+      <nav className='d-flex justify-content-between align-items-center p-3'> 
+        <p className='mb-0'>JOHN DOE</p>
+        <div className='ms-auto d-flex'>
+        <NavLink 
+              to="/" 
+              className={({ isActive }) => isActive ? "mx-2 active text-uppercase text-decoration-none" : "mx-2 text-uppercase text-decoration-none"}
+            >
+              home
+            </NavLink>
+            <NavLink 
+              to="/services" 
+              className={({ isActive }) => isActive ? "mx-1 active text-uppercase text-decoration-none" : "mx-1 text-uppercase text-decoration-none"}
+            >
+              services
+            </NavLink>
+            <NavLink 
+              to="/portfolio" 
+              className={({ isActive }) => isActive ? "mx-1 active text-uppercase text-decoration-none" : "mx-1 text-uppercase text-decoration-none"}
+            >
+              portfolio
+            </NavLink>
+            <NavLink 
+              to="/contact" 
+              className={({ isActive }) => isActive ? "mx-1 active text-uppercase text-decoration-none" : "mx-1 text-uppercase text-decoration-none"}
+            >
+              contact
+            </NavLink>
+            <NavLink 
+              to="/mentions" 
+              className={({ isActive }) => isActive ? "mx-1 active text-uppercase text-decoration-none" : "mx-1 text-uppercase text-decoration-none"}
+            >
+              mentions légales
+            </NavLink>
+        </div>
       </nav>
+    </div>
+    <div className="content">
       <Routes>
-        <Route path="/" element={<HOME />} />
-        <Route path="/services" element={<SERVICES />} />
-        <Route path="/portfolio" element={<PORTFOLIO />} />
-        <Route path="/contact" element={<CONTACT />} />
-        <Route path="/mentions" element={<MENTIONS_LEGALES />} />
+        <Route path='/' element={<HOME />} />
+        <Route path='/services' element={<SERVICES />} />
+        <Route path='/portfolio' element={<PORTFOLIO />} />
+        <Route path='/contact' element={<CONTACT />} />
+        <Route path='/mentions' element={<MENTIONS_LEGALES />} />
       </Routes>
+    </div>
     </div>
   );
 }
 
+// terminer le header
+// ajouter un footer
 
